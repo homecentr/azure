6db44198844117d82830be5b79c362cbf96fc1f4
Lukas Holota <lholota@vistaprint.com>
2022-12-03 18:35:25 +0100

    App consent

--- terraform/azuread-app-proxmox.tf
+++ terraform/azuread-app-proxmox.tf
@@ -25,3 +25,9 @@ resource "azuread_application" "proxmox" {
     requested_access_token_version = "2"
   }
 }
+
+resource "azuread_application_pre_authorized" "proxmox" {
+  application_object_id = azuread_application.proxmox.object_id
+  authorized_app_id     = azuread_application.proxmox.application_id
+  permission_ids        = ["37f7f235-527c-4136-accd-4a02d197296e", "7427e0e9-2fba-42fe-b0c0-848c9e6a8182"]
+}
