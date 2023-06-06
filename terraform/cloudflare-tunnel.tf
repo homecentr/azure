692be0423332d7b1c4d9e37732e6e2aeec4e7ee9
LH <me@lholota.com>
2023-06-07 01:21:45 +0200

    fix: Cloudflare tunnel secret encoding

--- terraform/cloudflare-tunnel.tf
+++ terraform/cloudflare-tunnel.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_tunnel" "kubernetes" {
   account_id = data.sops_file.secrets.data["cloudflare_account_id"]
   name       = "Kubernetes${var.display_name_environment_suffix}"
-  secret     = data.sops_file.secrets.data["cloudflare_tunnel_secret"]
+  secret     = base64encode(data.sops_file.secrets.data["cloudflare_tunnel_secret"])
 }
