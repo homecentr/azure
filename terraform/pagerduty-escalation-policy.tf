5b14c53c5b05011b60198105afd0bad54c4376ab
LH <me@lholota.com>
2023-08-17 10:21:31 +0200

    fix

--- terraform/pagerduty-escalation-policy.tf
+++ terraform/pagerduty-escalation-policy.tf
@@ -1,7 +1,6 @@
 resource "pagerduty_escalation_policy" "default" {
   name        = "Default"
   description = "Default escalation policy for all services"
-  num_loops   = var.pagerduty_escalation_policy_loops
 
   rule {
     escalation_delay_in_minutes = 3
