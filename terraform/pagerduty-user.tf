810895fc05073615421cd2fabfb7c6ae66cc0df7
LH <me@lholota.com>
2023-10-04 18:06:45 +0200

    debug 3

--- terraform/pagerduty-user.tf
+++ terraform/pagerduty-user.tf
@@ -1,3 +1,13 @@
+resource "null_resource" "terraform-debug" {
+  provisioner "local-exec" {
+    command = "echo $VARIABLE1"
+
+    environment = {
+        VARIABLE1 = length(var.pagerduty_api_token)
+    }
+  }
+}
+
 data "pagerduty_user" "me" {
   email = sensitive(data.sops_file.secrets.data["pagerduty_user_email"])
 }
