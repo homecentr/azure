cd048f66db469c569bc445576e0169865b827ed6
Lukas Holota <me@lholota.com>
2024-04-19 19:48:01 +0200

    Fix 3

--- terraform/cloudflare-app-tunnel-health.tf
+++ terraform/cloudflare-app-tunnel-health.tf
@@ -1,6 +1,4 @@
 resource "cloudflare_access_policy" "service_token" {
-  for_each = { for each in var.cloudflare_apps : each.display_name => each }
-
   application_id = cloudflare_access_application.apps["Cloudflare Tunnel Healthcheck"].id
   account_id     = sensitive(data.sops_file.secrets.data["cloudflare_account_id"])
   name           = "Service Token${var.display_name_environment_suffix}"
