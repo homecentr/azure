ae149cc79fb17cc60fe55796919c3718ead085d6
Lukas Holota <me@lholota.com>
2024-04-21 10:48:12 +0200

    Service auth debug 1

--- terraform/cloudflare-app-tunnel-health.tf
+++ terraform/cloudflare-app-tunnel-health.tf
@@ -3,7 +3,7 @@ resource "cloudflare_access_policy" "service_token" {
   account_id     = sensitive(data.sops_file.secrets.data["cloudflare_account_id"])
   name           = "Service Token${var.display_name_environment_suffix}"
   precedence     = "3"
-  decision       = "allow"
+  decision       = "non_identity"
 
   include {
     service_token = [var.cloudflare_health_service_token_name]
