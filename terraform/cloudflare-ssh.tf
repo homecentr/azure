f714dd4b4a286d38d3fa1447e01910de94110b6c
Lukas Holota <me@lholota.com>
2024-11-06 14:49:22 +0100

    feat: Apps deleted again

--- terraform/cloudflare-ssh.tf
+++ terraform/cloudflare-ssh.tf
@@ -9,22 +9,22 @@
 #   proxied = true
 # }
 
-resource "cloudflare_access_application" "ssh" {
-  for_each = { for each in var.cloudflare_ssh_hosts : each.hostname => each }
+# resource "cloudflare_access_application" "ssh" {
+#   for_each = { for each in var.cloudflare_ssh_hosts : each.hostname => each }
 
-  account_id                = sensitive(data.sops_file.secrets.data["cloudflare_account_id"])
-  name                      = "SSH / ${each.key}.${var.cloudflare_apps_root_domain}"
-  domain                    = "${each.key}.${var.cloudflare_apps_root_domain}"
-  type                      = "self_hosted"
-  session_duration          = "24h"
-  auto_redirect_to_identity = true
-  allowed_idps              = [cloudflare_access_identity_provider.azuread.id]
-  skip_interstitial         = true
+#   account_id                = sensitive(data.sops_file.secrets.data["cloudflare_account_id"])
+#   name                      = "SSH / ${each.key}.${var.cloudflare_apps_root_domain}"
+#   domain                    = "${each.key}.${var.cloudflare_apps_root_domain}"
+#   type                      = "self_hosted"
+#   session_duration          = "24h"
+#   auto_redirect_to_identity = true
+#   allowed_idps              = [cloudflare_access_identity_provider.azuread.id]
+#   skip_interstitial         = true
 
-  policies = [
-    cloudflare_access_policy.administrators.id
-  ]
-}
+#   policies = [
+#     cloudflare_access_policy.administrators.id
+#   ]
+# }
 
 ### Prefixed
 resource "cloudflare_record" "ssh_prefixed" {
