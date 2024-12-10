ac479c71dc11cff1042749b18c5ba5ca74b63c9e
Lukas Holota <me@lholota.com>
2024-12-10 22:12:50 +0100

    fix: Argo CD fixes

--- terraform/azuread-app-argocd.tf
+++ terraform/azuread-app-argocd.tf
@@ -3,6 +3,43 @@ resource "azuread_application" "argocd" {
   sign_in_audience = "AzureADMyOrg"
   logo_image       = filebase64("../icons/argocd.png")
 
+  web {
+    redirect_uris = [
+      "https://argocd.${var.root_domain}/auth/callback"
+    ]
+  }
+
+  required_resource_access {
+    resource_app_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+
+    # offline_access and openid scopes
+    resource_access {
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["openid"]
+      type = "Scope"
+    }
+
+    resource_access {
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["offline_access"]
+      type = "Scope"
+    }
+  }
+
+  optional_claims {
+    access_token {
+      name = "groups"
+    }
+
+    id_token {
+      name = "groups"
+    }
+
+    saml2_token {
+      name = "groups"
+    }
+  }
+
+  group_membership_claims = ["SecurityGroup", "ApplicationGroup"]
+
   api {
     requested_access_token_version = "2"
   }
